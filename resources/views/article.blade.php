578ceb89e512b20f4ffb536d25acbecdd894c1d2
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-17 10:04:10 +0200

    new routes with arguments

---
+++ resources/views/article.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+	<title>Les articles</title>
+</head>
+<body>
+    <h1>Page article</h1>
+	<p>C'est l'article n° <?php echo $numero ?></p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,21 @@ Route::get('/', function () {
 // Route::get('2', function() { return 'Je suis la page 2 !'; });
 // Route::get('3', function() { return 'Je suis la page 3 !'; });
 
-Route::get('{n}', function($n) {
-	return 'Je suis la page ' . $n . ' !'; 
+// Route::get('{n}', function($n) {
+// 	return 'Je suis la page ' . $n . ' !'; 
+// });
+
+
+Route::get('{n}', function($n) { 
+    return response('Je suis la page ' . $n . ' !', 200); 
+})->where('n', '[1-3]');
+
+Route::get('/vue1', function()
+{
+    return view('view1');
 });
+
+
+Route::get('article/{n}', function($n) { 
+    return view('article')->with('numero', $n); 
+})->where('n', '[0-9]+');
