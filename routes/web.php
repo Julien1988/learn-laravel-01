000e3ba28b1138b6a96c234007a30e3844491dc5
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-17 09:43:02 +0200

    new branch dev-01

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('1', function() { return 'Je suis la page 1 !'; });
+// Route::get('2', function() { return 'Je suis la page 2 !'; });
+// Route::get('3', function() { return 'Je suis la page 3 !'; });
+
+Route::get('{n}', function($n) {
+	return 'Je suis la page ' . $n . ' !'; 
+});
